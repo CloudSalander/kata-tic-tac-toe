a43845f28aa42c5ffd177160ba723e096fbd8914
CloudSalander <ruben.as532@gmail.com>
2024-10-17 10:51:08 +0200

    What I did in 30 minutes

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 
+define('MATRIX_LENGTH', 3);
+
 $game1 = [["X","O","O"],["O","X","O"],["-","O","X"]];
 $game2 = [["O","X","-"],["O","X","-"],["O","-","-"]];
 $game3 = [["O","X","O"],["X","O","X"],["X","X","O"]];
@@ -8,11 +10,21 @@ $game4 = [["-","-","-"],["O","O","-"],["X","X","X"]];
 //TODO: Array of checker functions?
 
 function checkRows(array $game): string | bool {
-    for($i = 0; $i < sizeof($game); ++$i) {
+    for($i = 0; $i < MATRIX_LENGTH; ++$i) {
         $row = $game[$i];
         if(($row[0] == $row[1]) && ($row[1] == $row[2]) && $row[0] != "-") return $row[0];
     }
     return false;
 }
 
-var_dump(checkRows($game4));
+function checkColumns(array $game): string | bool {
+    for($j = 0; $j < MATRIX_LENGTH; ++$j) {
+        $column = $game[$j];
+        var_dump($column);
+        if(($column[0] == $column[1]) && ($column[1] == $column[2]) && $column[0] != "-")  return $column[0];
+    }
+    return false;
+}
+
+var_dump(checkColumns($game1));
+var_dump(checkColumns($game2));
