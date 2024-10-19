4dedff67c85666d330c63c98f23d6d245e73f6b4
CloudSalander <ruben.as532@gmail.com>
2024-10-19 02:24:44 +0200

    Solved with anonymous functions

--- index.php
+++ index.php
@@ -7,36 +7,38 @@ $game2 = [["O","X","-"],["O","X","-"],["O","-","-"]];
 $game3 = [["O","X","O"],["X","O","X"],["X","X","O"]];
 $game4 = [["-","-","-"],["O","O","-"],["X","X","X"]];
 
-//TODO: Array of checker functions?
+$games = [$game1,$game2,$game3,$game4];
 
-function checkRows(array $game): string | bool {
-    for($i = 0; $i < MATRIX_LENGTH; ++$i) {
-        $row = $game[$i];
-        if(($row[0] == $row[1]) 
-            && ($row[1] == $row[2]) 
-            && $row[0] != "-") return $row[0];
+$checker_functions = [
+    'row' =>  function(array $game): string | bool {
+        for($i = 0; $i < MATRIX_LENGTH; ++$i) {
+            $row = $game[$i];
+            if(($row[0] == $row[1]) 
+                && ($row[1] == $row[2]) 
+                && $row[0] != "-") return $row[0];
+        }
+        return false;
+    },
+    'column' => function(array $game): string | bool {
+        for($j = 0; $j < MATRIX_LENGTH; ++$j) {
+            if(($game[0][$j] == $game[1][$j]) 
+                && $game[1][$j] == $game[2][$j] 
+                && $game[0][$j] != "-")  return $game[0][$j];
+        }
+        return false;
+    },
+    'diagonal' => function(array $game): string | bool {
+        if ((($game[0][0] == $game[1][1]) && ($game[1][1] == $game[2][2]) && $game[0][0] != "-")
+             ||
+            (($game[0][2] == $game[1][1]) && ($game[1][1] == $game[2][0]) && $game[0][2] != "-"))
+              return $game[1][1];
+        return false;
     }
-    return false;
-}
+];
 
-function checkColumns(array $game): string | bool {
-    for($j = 0; $j < MATRIX_LENGTH; ++$j) {
-        if(($game[0][$j] == $game[1][$j]) 
-            && $game[1][$j] == $game[2][$j] 
-            && $game[0][$j] != "-")  return $game[0][$j];
+foreach($games as $game) {
+    foreach($checker_functions as $key => $checker_function) {
+        $result = $checker_function($game);
+        if($result !== false) echo "Winner is ".$result." by ".$key.PHP_EOL;
     }
-    return false;
 }
-
-function checkDiagonals(array $game): string | bool {
-    if ((($game[0][0] == $game[1][1]) && ($game[1][1] == $game[2][2]) && $game[0][0] != "-")
-         ||
-        (($game[0][2] == $game[1][1]) && ($game[1][1] == $game[2][0]) && $game[0][2] != "-"))
-          return $game[1][1];
-    return false;
-}
-
-var_dump(checkDiagonals($game1));
-var_dump(checkDiagonals($game2));
-var_dump(checkDiagonals($game3));
-var_dump(checkDiagonals($game4));
